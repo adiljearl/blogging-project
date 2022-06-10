8e60f755c9c2bb7a5bf7b009e3c87837ce2cb68d
adiljearl <mdadil3195@gmail.com>
2022-06-10 15:22:20 +0530

    Major Changes

--- app.js
+++ app.js
@@ -4,8 +4,12 @@ const exphbs = require("express-handlebars");
 const path = require("path");
 const Handlebars = require("handlebars");
 const mongoose = require("mongoose");
-// const upload = require("express-fileupload");
+const bodyParser = require("body-parser");
+const methodOverride = require("method-override");
+const upload = require("express-fileupload");
 const { mongoDbUrl } = require("./config/config");
+const { select, generateTime } = require("./helpers/handlebars-helpers");
+const passport = require("passport");
 mongoose.set("useCreateIndex", true);
 mongoose.set("useFindAndModify", false);
 
@@ -14,7 +18,7 @@ mongoose.Promise = global.Promise;
 mongoose
   .connect(mongoDbUrl)
   .then(db => {
-    console.log("MONGO CONNECTED");
+    console.log("MONGO CONNECTED !!");
   })
   .catch(error => console.log("not connected"));
 
@@ -24,22 +28,33 @@ const {
 const insecureHandlebars = allowInsecurePrototypeAccess(Handlebars);
 app.use(express.static(path.join(__dirname, "public")));
 
+// app.use(passport.initialize());
+// app.use(passport.session());
+
 app.engine(
   "handlebars",
-  exphbs({
-    handlebars: allowInsecurePrototypeAccess(Handlebars)
-  })
+  exphbs({handlebars: allowInsecurePrototypeAccess(Handlebars),helpers: { generateTime: generateTime }})
 );
+//body Parser
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(upload());
 app.set("view engine", "handlebars");
+
+app.use(methodOverride("_method"));
 
 const home = require("./routes/home/index");
 const admin = require("./routes/admin/index");
+const post = require("./routes/post/index");
 const login = require("./routes/login/index");
 const posts = require("./routes/admin/posts");
 const profile = require("./routes/admin/profile");
 
 app.use("/", home);
 app.use("/admin", admin);
+app.use("/post", post);
 app.use("/login", login);
 app.use("/admin/posts", posts);
 app.use("/admin/profile", profile);
--- models/Post.js
+++ models/Post.js
@@ -2,26 +2,31 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const PostSchema = new Schema(
-  {
-    title: {
-      type: String,
-      required: true
-    },
-
-    body: {
-      type: String,
-      required: true
-    },
-    file: {
-      type: String
-    },
-    date: {
-      type: Date,
-      default: Date.now()
-    }
+const PostSchema = new Schema({
+  title: {
+    type: String,
+    required: true
   },
-  { usePushEach: true }
-);
+
+  body: {
+    type: String,
+    required: true
+  },
+  file: {
+    type: String
+  },
+  label: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now()
+  },
+  isHeadline: {
+    type: Boolean,
+    default: false
+  }
+});
 
 module.exports = mongoose.model("posts", PostSchema);
